947baf859fbbcb0d412929563c3a55c9ed5e9edb
Labib <risenotfall123@gmail.com>
2023-06-17 18:37:40 -0400

    implemented spotify API

--- src/components/EmojiMoodTracker.js
+++ src/components/EmojiMoodTracker.js
@@ -1,55 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import './EmojiMoodTracker.css';
 
+const CLIENT_ID = "5a167d608d4e47669c046f3d0dbbfa86";
+const CLIENT_SECRET = "6767ff8ce13b4c72908acb81d3bea6d0";
+const SCOPES = "user-library-read"; 
+
 function EmojiMoodTracker() {
   const [selectedMood, setSelectedMood] = useState('');
-  const [moodHistory, setMoodHistory] = useState([]);
-  const [recommendedMusic, setRecommendedMusic] = useState('');
+  //const [recommendedMusic, setRecommendedMusic] = useState('');
+  const [accessToken, setAccessToken] = useState('')
 
   useEffect(() => {
-    const storedMoodHistory = localStorage.getItem('moodHistory');
-    if (storedMoodHistory) {
-      setMoodHistory(JSON.parse(storedMoodHistory));
+    //API access token
+    var authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&scope=${SCOPES}`
     }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token))
   }, []);
 
-  useEffect(() => {
-    localStorage.setItem('moodHistory', JSON.stringify(moodHistory));
-  }, [moodHistory]);
+  async function button() {
+    console.log("Button pressed ");
+
+    // get request using track to get user's saved tracks
+    var trackParameters = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }
+    var savedTracks = await fetch('https://api.spotify.com/v1/me/tracks?market=US&limit=10&offset=0', trackParameters)
+      .then(response => response.json())
+      .then(data => console.log(data)) 
+    
+    // get request with item's ID to grab all the tracks' ids
+    /*
+    var savedTracks = await fetch('https://api.spotify.com/v1/audio-features?ids=2takcwOaAZWiXQijPHIx7B', trackParameters)
+      .then(response => response.json())
+      .then(data => console.log(data)) 
+    */
+    // get request with track's ID to grab the audio details
+
+    // algorithm to determine what track to display based on the mood
+
+    // Display the song based on emoji mood
+  }
 
   const handleMoodSelect = (mood) => {
-    const updatedMoodHistory = [...moodHistory, mood];
-    setMoodHistory(updatedMoodHistory);
+    button(mood);
     setSelectedMood(mood);
-    fetchMusicRecommendation(mood);
-  };
-
-  const handleDeleteMood = (index) => {
-    const updatedMoodHistory = [...moodHistory];
-    updatedMoodHistory.splice(index, 1);
-    setMoodHistory(updatedMoodHistory);
-  };
-
-  const handleClearMoodHistory = () => {
-    setMoodHistory([]);
-  };
-
-  const fetchMusicRecommendation = (mood) => {
-    // Make an API call to fetch music recommendation based on mood
-    // Replace the URL with the actual endpoint of your Music Mood Analyzer API
-    fetch(`https://your-music-mood-analyzer-api.com/recommendations?mood=${mood}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setRecommendedMusic(data.music);
-      })
-      .catch((error) => {
-        console.error('Error fetching music recommendation:', error);
-      });
   };
 
   return (
     <div className="emoji-mood-tracker">
-      <h1 className="title">Emoji Mood Tracker</h1>
+      <h1 className="title">Moodify</h1>
 
       <div className="mood-select-container">
         <h2>Select your mood:</h2>
@@ -75,35 +85,14 @@ function EmojiMoodTracker() {
       {selectedMood && (
         <div className="mood-details-container">
           <h2>Your selected mood: {selectedMood}</h2>
-          <h3>Mood History:</h3>
-          {moodHistory.length > 0 ? (
-            <div>
-              <ul className="mood-history-list">
-                {moodHistory.map((mood, index) => (
-                  <li key={index}>
-                    {mood}{' '}
-                    <button
-                      className="delete-button"
-                      onClick={() => handleDeleteMood(index)}
-                    >
-                      Delete
-                    </button>
-                  </li>
-                ))}
-              </ul>
-              <button onClick={handleClearMoodHistory}>
-                Clear Mood History
-              </button>
-            </div>
-          ) : (
-            <p>No mood history available.</p>
-          )}
+          {/*}
           <h3>Recommended Music:</h3>
           {recommendedMusic ? (
             <p>{recommendedMusic}</p>
           ) : (
             <p>Loading music recommendation...</p>
           )}
+          */}
         </div>
       )}
     </div>
